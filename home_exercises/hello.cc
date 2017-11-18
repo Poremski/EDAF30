d41a1773e79ee0166c8a15ad44041e963074946f
Javier Poremski <javier@poremski.se>
2017-11-18 23:11:58 +0100

    Update HelloWorld!

--- home_exercises/hello.cc
+++ home_exercises/hello.cc
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <string>
 
-using std::cout;
-using std::endl;
+class HelloClass {
+  public:
+    HelloClass() {
+      name = "World";
+    }
+    void setHello(std::string x) {
+      hello = x;
+    }
+    void setName(std::string x) {
+      name = x;
+    }
+    std::string getHello() {
+      return hello;
+    }
+    std::string getName() {
+      return name;
+    };
+    std::string getMessage() {
+      return hello + " " + name + "!";
+    }
+    void printMessage() {
+      std::cout << getMessage() << std::endl;
+    }
+  private:
+    std::string hello;
+    std::string name;
+
+};
 
 int main()
 {
-  cout << "Hello World!" << endl;
+  HelloClass hc;
+  hc.setHello("Hello");
+  std::cout << hc.getHello() + " " + hc.getName() + "!" << std::endl;
+  std::cout << hc.getMessage() << std::endl;
+  hc.setName("Javier");
+  hc.printMessage();
+
+
   return 0;
 }
