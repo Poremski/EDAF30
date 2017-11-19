002c5dbd6fed340f0ffab7c4fc48350ca575fdb3
Javier Poremski <javier@poremski.se>
2017-11-19 01:11:40 +0100

    Create luhn.h

---
+++ home_exercises/luhn.h
@@ -0,0 +1,15 @@
+#ifndef LUHN_H
+#define LUHN_H
+
+class Luhn {
+    public:
+        Luhn(std::string sequence);
+        void setSequence(std::string sequence);
+        int getChecksum();
+        int getNextDigit();
+    private:
+        std::string _sequence;
+        int calc(bool reverse = false);
+};
+
+#endif // LUHN_H
