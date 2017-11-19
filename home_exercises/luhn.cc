bc117d02bb7f6f4d0148b48e5eb8625f13799cd4
Javier Poremski <javier@poremski.se>
2017-11-19 01:11:55 +0100

    Update luhn.cc

--- home_exercises/luhn.cc
+++ home_exercises/luhn.cc
@@ -1,49 +1,51 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include "luhn.h"
 
-class LuhnClass {
-  public:
-    LuhnClass(std::string sequence) {
-      setSequence(sequence);
+Luhn::Luhn(std::string sequence)
+{
+    Luhn::setSequence(sequence);
+}
+
+void Luhn::setSequence(std::string sequence)
+{
+    Luhn::_sequence = sequence;
+}
+int Luhn::getChecksum()
+{
+    return Luhn::calc();
+}
+int Luhn::getNextDigit()
+{
+    int c = 10 - calc(true);
+    if (c > 9) return 0;
+    else return c;
+}
+int Luhn::calc(bool reverse)
+{
+    int sum = 0;
+    int nDigits = Luhn::_sequence.length();
+    int parity = nDigits % 2;
+    for (int i = 0; i < nDigits; ++i) {
+        int digit = _sequence[i] - '0';
+        bool multi = (i % 2 == parity);
+        if (reverse) multi = (i % 2 != parity);
+        if (multi) {
+            digit = digit * 2;
+            if (digit > 9) digit -= 9;
+        }
+        sum += digit;
     }
-    void setSequence(std::string sequence) {
-      _sequence = sequence;
-    }
-    int getChecksum() {
-      return calc();
-    }
-    int getNextDigit() {
-        int c = 10 - calc(true);
-        if (c > 9) return 0;
-        else return c;
-    }
-  private:
-      std::string _sequence;
-      int calc(bool reverse = false) {
-          int sum = 0;
-          int nDigits = _sequence.length();
-          int parity = nDigits % 2;
-          for (int i = 0; i < nDigits; ++i) {
-              int digit = _sequence[i] - '0';
-              bool multi = (i % 2 == parity);
-              if (reverse) multi = (i % 2 != parity);
-              if (multi) {
-                  digit = digit * 2;
-                  if (digit > 9) digit -= 9;
-              }
-              sum += digit;
-          }
-          return sum % 10;
-      }
-};
+    return sum % 10;
+}
 
 int main() {
   std::string sequence;
   std::cout << "Digit sequence: ";
   std::getline(std::cin, sequence);
 
-  LuhnClass lc(sequence);
+  Luhn::Luhn lc(sequence);
 
   std::cout << "Luhn checksum mod 10: " << lc.getChecksum() << std::endl;
   std::cout << "Next check digit: " << lc.getNextDigit() << std::endl;
